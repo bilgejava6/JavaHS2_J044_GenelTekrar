2a26f52f073c7d69bac4fab4c40360eee1f7e1be
sercan94 <sercan.imir1994@gmail.com>
2024-12-21 12:18:17 +0300

    Sercan

--- src/main/java/com/sercan/Degiskenler/Runner_Kolay.java
+++ src/main/java/com/sercan/Degiskenler/Runner_Kolay.java
@@ -17,5 +17,105 @@ public class Runner_Kolay {
          * 	10.	byte türünde bir değişken tanımlayıp 127 değeri verin. Daha büyük bir değer atarsanız ne olur?
          * 	*/
 
+
+
     }
+    public void cevap1(){
+        System.out.println("""
+                1. sorunun çözümü
+                """);
+        int tamSayi=10;
+        System.out.println(tamSayi);
+        System.out.println("-------------------------------------------");
+    }
+
+    public void cevap2(){
+        System.out.println("""
+                2. sorunun çözümü
+                """);
+        double deger=3.14d;
+        System.out.println(deger);
+        System.out.println("--------------------------------------------");
+    }
+
+    public void cevap3(){
+        System.out.println("""
+                3.sorunun cevabı
+                """);
+        String ad="Sercan";
+        System.out.println("Merhaba "+"["+ad+"]!");
+        System.out.println("----------------------------------------------");
+    }
+
+    public void cevap4(){
+        System.out.println("""
+                4.sorunun cevabı
+                """);
+        boolean dogruMu=true;
+        System.out.println(dogruMu);
+        System.out.println("------------------------------------------------");
+    }
+
+    public void cevap5(){
+        System.out.println("""
+                5. sorunun cevabı
+                """);
+        char ilkHarf='S';
+        System.out.println(ilkHarf);
+        System.out.println("-------------------------------------------------");
+    }
+
+    public void cevap6(){
+        System.out.println("""
+                6. sorunun cevabı
+                """);
+        int x=5,y=7;
+        int toplam=x+y;
+        System.out.println("Toplam : "+toplam);
+        System.out.println("-------------------------------------------------");
+    }
+
+    public void cevap7(){
+        System.out.println("""
+                7. sorunun cevabı
+                """);
+        Integer sayi=null;
+        sayi=100;
+        System.out.println(sayi);
+        System.out.println("-------------------------------------------------");
+    }
+
+    public void cevap8(){
+        System.out.println("""
+                8. sorunun cevabı
+                """);
+        int  k;
+        System.out.println("variable k might not have been initialized");
+        System.out.println("değer atanmadığı için başlatılamadığını ifade ediyor");
+        System.out.println("-------------------------------------------------");
+    }
+
+    public void cevap9(){
+        System.out.println("""
+                9. sorunun cevabı
+                """);
+        float j=2.5f;
+        System.out.println(j);
+        System.out.println("-------------------------------------------------");
+    }
+
+    public void cevap10(){
+        System.out.println("""
+                10. sorunun cevabı
+                """);
+        byte byteDegeri=127;
+        System.out.println(byteDegeri);
+        System.out.println("""
+                        byte -128 ile 127 aralığında değer alır daha büyük değer verirsek int dönüştürmemizi ister byte şeklinde istersek
+                        -128 ile 127 aralığında o sayiya denk gelen değeri döner
+                        """);
+
+        System.out.println("-------------------------------------------------");
+    }
+
 }
--- src/main/java/com/sercan/Degiskenler/Runner_Orta.java
+++ src/main/java/com/sercan/Degiskenler/Runner_Orta.java
@@ -1,5 +1,7 @@
 package com.sercan.Degiskenler;
 
+import java.util.Scanner;
+
 public class Runner_Orta {
     public static void main(String[] args) {
         /**
@@ -15,6 +17,75 @@ public class Runner_Orta {
          * 	9.	Bir short değişken oluşturup bir int değişkenine atayın. Bu işlem sırasında bir hata alır mısınız?
          * 	*/
 
+    }
+    public static void cevap1(){
+        System.out.print("Yasınız  :");
+        int yas=new Scanner(System.in).nextInt();
+        System.out.println("Yas : "+yas );
 
+    }
+
+    public static void  cevap2(){
+        final int deger=5;
+        System.out.println(deger);
+        //  deger=15;
+        System.out.println("cannot assign a value to final variable deger");
+       /** System.out.println("""
+                final kullanıldığında o değişkenin degeri bir dahadeğiştirilemez.
+                Sabit olanlarda kullanılır.
+                """);
+        */
+    }
+
+    public static void cevap3(){
+        String ad="Sercan";
+        String soyad="İmir";
+        System.out.println(ad.concat(" "+soyad));
+    }
+
+    public static void cevap4(){
+        double sayi=3.14;
+        int sayi2=5;
+        System.out.println(sayi+sayi2);
+    }
+
+    public static void cevap5(){
+        String deger="123";
+        int deger2= Integer.parseInt(deger);
+        int deger3=5;
+        int toplam=deger2+deger3;
+        System.out.println("Toplam : "+toplam);
+    }
+
+    public static void cevap6(){
+        long    sayi=9_999_999_999L;
+        System.out.println(sayi);
+        // hata almayız
+    }
+
+    public static void cevap7(){
+        int sayi=5;
+        float sayi2=(float)sayi;
+        String sayi3=String.valueOf(sayi2);
+
+    }
+
+    public static void cevap8(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Lütfen adınızı giriniz ...:");
+        String kelime=sc.nextLine();
+        int kelimeUzunlugu=kelime.length();
+        System.out.println(kelimeUzunlugu);
+
+
+    }
+
+    public static void  cevap9(){
+        short sayi=15;
+        int sayi2=sayi;
+        System.out.println(sayi2);
+        /**
+         *  short 16 bit ,int 32 bit yani daha geniş olduğundan bir problem olmaz
+         */
     }
 }
