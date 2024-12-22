a2853d3dbd6d4e27bbf59c33038162c65375addd
bekoberkan <berkans.akbuluts@gmail.com>
2024-12-22 16:10:55 +0300

    Kolay ve orta tamamnlandi Collections

---
+++ src/main/java/com/mberkan/RunnerCollectionsZor.java
@@ -0,0 +1,25 @@
+package com.mberkan;
+
+import java.util.Comparator;
+import java.util.TreeMap;
+
+public class RunnerCollectionsZor {
+    public static void main(String[] args) {
+    cevap1();
+    }
+    static void cevap1() {
+        Comparator<String> reverseComparator = Comparator.reverseOrder();
+
+        TreeMap<String, Integer> treeMap = new TreeMap<>(reverseComparator);
+
+        treeMap.put("A", 1);
+        treeMap.put("B", 2);
+        treeMap.put("C", 3);
+        treeMap.put("D", 4);
+        treeMap.put("E", 5);
+
+        for (String key : treeMap.keySet()) {
+            System.out.println("Anahtar: " + key + ", Deger: " + treeMap.get(key));
+        }
+        }
+    }
