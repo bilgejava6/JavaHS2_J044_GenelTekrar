32cc5cab79cb5fc72353c9d740113acb85b7e92c
Bekir Can <bcyumrutepe@gmail.com>
2024-12-22 12:36:32 +0300

    Bekir Can çalışma sorular oop

---
+++ src/main/java/com/bekircan/oop/Ogrenci.java
@@ -0,0 +1,9 @@
+package com.bekircan.oop;
+
+public class Ogrenci extends Kisi{
+//    @Override
+//    public void soru4() {
+//        System.out.println("Bu final metodu değiştirilmek istendi.");
+//    }
+    // error: soru4() in Ogrenci cannot override soru4() in Kisi hatası alınıyor.
+}
