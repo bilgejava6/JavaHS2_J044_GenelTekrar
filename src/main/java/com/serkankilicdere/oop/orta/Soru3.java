73e833352fac79c87f4680a3c10280988ace66de
serkankilicdere <serkan.kilicdere@outlook.com>
2024-12-22 15:02:21 +0300

    oop orta devam ediyor

---
+++ src/main/java/com/serkankilicdere/oop/orta/Soru3.java
@@ -0,0 +1,11 @@
+package com.serkankilicdere.oop.orta;
+
+public interface Soru3 {
+    public void soru3();
+}
+
+class Soru3Impl implements Soru3 {
+    public void soru3() {
+        System.out.println("Soru3Impl");
+    }
+}
