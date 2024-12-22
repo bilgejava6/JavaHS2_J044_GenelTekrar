688704b7e5defd0f96a4ab962a61b6aa24cafa34
Onur Cinstas <onur@cnsgames.com>
2024-12-22 14:39:39 +0300

    oop zor 7. soruya kadar

--- src/main/java/com/onur/oop/zor/ZorRunner.java
+++ src/main/java/com/onur/oop/zor/ZorRunner.java
@@ -1,4 +1,27 @@
 package com.onur.oop.zor;
 
+import com.onur.oop.zor.alti.Person;
+import com.onur.oop.zor.dort.Singleton;
+
 public class ZorRunner {
+
+    public static void main(String[] args) {
+
+        //4. soru
+        Singleton singleton = Singleton.getInstance();
+        singleton.showMessage();
+
+        //6. soru
+        Person person1 = new Person();
+        Person person2 = new Person("John");
+        Person person3 = new Person("Jane", 25);
+        Person person4 = new Person("Alice", 30, "123 Main St");
+
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+        System.out.println(person4);
+
+    }
+
 }
